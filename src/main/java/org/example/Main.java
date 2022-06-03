3b37d7cdc1f60a02561987dfd4e6cec78ab13eec
James Keogh <jkeogh@adga.ca>
2022-06-03 14:26:48 -0400

    multiple line added

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -10,5 +10,18 @@ public class Main {
         System.out.println("line 4 from pc ");
 
 
+        System.out.println("line 2 from pc ");
+        System.out.println("line 3 from pc ");
+        System.out.println("line 4 from pc ");
+        System.out.println("line 2 from pc ");
+        System.out.println("line 3 from pc ");
+        System.out.println("line 4 from pc ");
+
+
+        System.out.println("line 2 from pc ");
+        System.out.println("line 3 from pc ");
+        System.out.println("line 4 from pc ");
+
+
     }
 }
