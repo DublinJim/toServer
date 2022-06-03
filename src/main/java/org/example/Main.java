180de89ee7c7789e6f4552b5f36bc429a138922f
James Keogh <jkeogh@adga.ca>
2022-06-03 13:35:03 -0400

    adds a line on 6

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,7 +3,7 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world! from a new angle");
-
+        System.out.println("now we add a line");
     System.out.println("line 2 from pc ");
     }
 }
