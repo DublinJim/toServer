7fb5f660380755781ac1c6c8651692670d4b5ed5
James Keogh <jkeogh@adga.ca>
2022-06-03 13:43:05 -0400

    see what this does

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,6 +4,9 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world! from a new angle");
         System.out.println("now we add a line");
+
     System.out.println("line 2 from pc ");
+
+        System.out.println("add another line 3");
     }
 }
