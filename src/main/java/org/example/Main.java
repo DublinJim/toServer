5be33b80be65e2818715f14132843470d5effb8f
James Keogh <jkeogh@adga.ca>
2022-06-08 10:34:04 -0400

    commit 5

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,17 +4,17 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world! from a new angle");
         System.out.println("conflict the line");
-    System.out.println("line 2 from pc ");
+    System.out.println("line 2 from pc ");// I want a commenty too
         System.out.println("line 2 from pc ");
         System.out.println("line 3 from pc ");
         System.out.println("line 4 from pc ");
 
-
+// I want a commenty too
 
         System.out.println("line 2 from pc ");
         System.out.println("line 3 from pc ");
         System.out.println("line 4 from pc ");
-
+// I want a commenty too
 
     }
 }
